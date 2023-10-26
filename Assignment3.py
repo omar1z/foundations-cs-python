03b76f5119d4eed24b80df6e01d9e0b9cc842b06
omar1z <omarzammar11@gmail.com>
2023-10-26 16:19:56 +0300

    menu in main

--- Assignment3.py
+++ Assignment3.py
@@ -1,17 +1,54 @@
-#ASSIGNMENT 3
-
+#ASSIGNMENT3
+#user's name
 print("HELLO MY FRIEND :)")
 username = input("Please enter your name :")
-print("Welcome dear _",username,"_")
+print("Welcome dear _", username, "_")
 
-print()
-print("1.Add Matrices")
-print("2.Check Rotation")
-print("3.Invert Dictionary")
-print("4.Convert Matrix to Dictionary")
-print("5.Check Palindrome")
-print("6.Search for an Element & Merge Sort")
-print("7.EXIT")
-print()
+def main():
+    #MENU
+    print()
+    print("1.Add Matrices")
+    print("2.Check Rotation")
+    print("3.Invert Dictionary")
+    print("4.Convert Matrix to Dictionary")
+    print("5.Check Palindrome")
+    print("6.Search for an Element & Merge Sort")
+    print("7.EXIT")
+    print()
 
-menuNb = int(input("Enter your choice please :"))
+    menuNb = int(input("Enter your choice please :"))
+
+
+
+
+
+main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
