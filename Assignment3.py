54ee88e7c8249e2533e4ce096f94a6dfc656f39e
omar1z <omarzammar11@gmail.com>
2023-10-26 16:55:51 +0300

    addMatrices fun taking values

--- Assignment3.py
+++ Assignment3.py
@@ -18,8 +18,27 @@ def main():
 
     menuNb = int(input("Enter your choice please :"))
 
+    if menuNb == 1:
+        addMatrices()
 
+def addMatrices():
+    k,i,j,x,y = 0,0,0,0,0
+    m1 = []
+    m2 = []
+    rows = int(input("Enter number of rows :"))
+    cols = int(input("Enter number of columns :"))
+    for i in range(rows):
+        a = []
+        for j in range(cols):
+            a.append(eval(input("Enter elements of the row of matrix1:")))
+        m1.append(a)
+    for i in range(rows):
+        b = []
+        for j in range(cols):
+            b.append(eval(input("Enter elements of the row of matrix2:")))
+        m2.append(b)
 
+    print(m1, "+", m2, "=")
 
 
 main()
