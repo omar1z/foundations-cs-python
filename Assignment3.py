21e69a345ad9a3f56dfdb8c23c24347b939de16a
omar1z <omarzammar11@gmail.com>
2023-10-26 14:08:14 +0300

    Putting the menu

--- Assignment3.py
+++ Assignment3.py
@@ -1,3 +1,17 @@
+#ASSIGNMENT 3
+
 print("HELLO MY FRIEND :)")
 username = input("Please enter your name :")
 print("Welcome dear _",username,"_")
+
+print()
+print("1.Add Matrices")
+print("2.Check Rotation")
+print("3.Invert Dictionary")
+print("4.Convert Matrix to Dictionary")
+print("5.Check Palindrome")
+print("6.Search for an Element & Merge Sort")
+print("7.EXIT")
+print()
+
+menuNb = int(input("Enter your choice please :"))
