ff935961c1c88d7b249ea745a5b71db770cd83fb
omar1z <omarzammar11@gmail.com>
2023-10-26 18:25:54 +0300

    func checkRotation(user will choose only one time nb of cols and rows and they will be swaped for second matrix)

--- Assignment3.py
+++ Assignment3.py
@@ -1,10 +1,10 @@
 #ASSIGNMENT3
-#user's name
-print("HELLO MY FRIEND :)")
-username = input("Please enter your name :")
-print("Welcome dear _", username, "_")
-
 def main():
+    # user's name
+    print("HELLO MY FRIEND :)")
+    username = input("Please enter your name :")
+    print("Welcome dear _", username, "_")
+
     #MENU
     print()
     print("1.Add Matrices")
@@ -21,7 +21,7 @@ def main():
     if menu_Nb == 1:
         addMatrices()
     elif menu_Nb == 2:
-        checkRotation()
+        print(checkRotation())
 
 def addMatrices():
     i,j = 0,0
@@ -30,11 +30,13 @@ def addMatrices():
     rows = int(input("Enter number of rows :"))
     cols = int(input("Enter number of columns :"))
     for i in range(rows):
+        print("row", i+1)
         a = []
         for j in range(cols):
             a.append(eval(input("Enter one element of the row of matrix1:")))
         m1.append(a)
     for i in range(rows):
+        print("row", i+1)
         b = []
         for j in range(cols):
             b.append(eval(input("Enter one element of the row of matrix2:")))
@@ -51,7 +53,40 @@ def addMatrices():
 
 
 def checkRotation():
-    
+    m1 = []
+    m2 = []
+    rows1 = int(input("Enter number of rows:"))
+    cols1 = int(input("Enter number of columns:"))
+
+    # Input for the first matrix
+    for i in range(rows1):
+        print("Row", i + 1)
+        a = []
+        for j in range(cols1):
+            a.append(eval(input("Enter one element of the row of matrix1:")))
+        m1.append(a)
+
+    # Create the second matrix with swapped rows and columns
+    rows2 = cols1
+    cols2 = rows1
+    for i in range(rows2):
+        print("Row", i + 1)
+        b = []
+        for j in range(cols2):
+            b.append(eval(input("Enter one element of the row of matrix2:")))
+        m2.append(b)
+
+    print(m1, " =? ", m2)
+    c = 0
+    for i in range(rows1):
+        for j in range(cols1):
+            if m1[i][j] == m2[j][i]:
+                c += 1
+
+    if c == rows1 * cols1:
+        return "MATRIX X IS A ROTATION OF MATRIX Y!"
+    else:
+        return "MATRIX X IS NOT A ROTATION OF MATRIX Y!"
 
 
 main()
