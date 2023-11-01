a97704aca9720b0edc62705a5df74d68a87597ad
omar1z <omarzammar11@gmail.com>
2023-11-01 10:21:59 +0200

    Displaying menu

--- Assignment_03_Omar_Zammar.py
+++ Assignment_03_Omar_Zammar.py
@@ -1,2 +1,15 @@
 # FSW (Simulating Student API Endpoints)
 
+def main():
+    print("1. Get Student by ID")
+    print("2. Get All Students")
+    print("3. Get Students by Major")
+    print("4. Add Student")
+    print("5. Find Common Majors")
+    print("6. Delete Student")
+    print("7. Calculate Average GPA")
+    print("8. Get Top Performers")
+    print("9. EXIT")
+
+
+main()
