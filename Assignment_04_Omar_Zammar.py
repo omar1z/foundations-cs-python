b28168e2386b57394e2f6aeaff04b08072cbe206
omar1z <omarzammar11@gmail.com>
2023-11-22 16:52:54 +0200

    constructor of class Task

--- Assignment_04_Omar_Zammar.py
+++ Assignment_04_Omar_Zammar.py
@@ -1,2 +1,16 @@
-# ASSIGNMENT 4
+# Task Manager
+
+class Task:
+    task_id = 0
+
+    def __init__(self, description, priority, completed=False):
+        Task.task_id += 1
+        self.task_id = Task.task_id
+        self.description = description
+        self.priority = priority
+        self.completed = completed
+
+        
+
+
 
