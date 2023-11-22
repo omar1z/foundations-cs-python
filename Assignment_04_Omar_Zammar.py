031e42724d4992e210d6983b1dbaf08184c786c6
omar1z <omarzammar11@gmail.com>
2023-11-22 17:01:10 +0200

    implementing setters

--- Assignment_04_Omar_Zammar.py
+++ Assignment_04_Omar_Zammar.py
@@ -3,7 +3,7 @@
 class Task:
     task_id = 0
 
-    def __init__(self, description, priority, completed=False):
+    def __init__(self, description, priority: int, completed=False):
         Task.task_id += 1
         self.task_id = Task.task_id
         self.description = description
@@ -21,6 +21,17 @@ class Task:
 
     def getStatus(self):
         return self.completed
+
+    # I won't define a method setter for task_id because id is stable & incremented
+
+    def setDescription(self, description):
+        self.description = description
+
+    def setPriority(self, priority):
+        self.priority = priority
+
+    def setStatus(self, completed):
+        self.completed = completed
 
 
 
